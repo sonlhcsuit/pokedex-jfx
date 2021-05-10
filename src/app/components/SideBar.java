3a8611cb0ae8e2c18ff17a1cc3aa6d6f16cc88e4
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-10 17:47:57 +0700

    side bar updated

--- src/app/components/SideBar.java
+++ src/app/components/SideBar.java
@@ -1,4 +1,21 @@
 package app.components;
 
-public class SideBar {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class SideBar extends VBox {
+	public SideBar() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setRoot(this);
+		loader.setControllerFactory((t) -> this);
+		String filename = this.getClass().getSimpleName() + ".fxml";
+		try {
+			loader.load(this.getClass().getResourceAsStream(filename));
+		} catch (IOException e){
+			throw new RuntimeException(e);
+		}
+
+	}
 }
