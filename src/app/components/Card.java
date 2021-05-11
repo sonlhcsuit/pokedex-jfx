81e705f162907c0e1af77fc27eed6d301dc85ff2
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-11 18:33:25 +0700

    update things for card

--- src/app/components/Card.java
+++ src/app/components/Card.java
@@ -1,14 +1,17 @@
 package app.components;
+
 import java.io.IOException;
+import java.util.Locale;
 
 import javafx.beans.DefaultProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.NamedArg;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.Parent;
 
@@ -16,49 +19,65 @@ import javafx.scene.Parent;
 @DefaultProperty(value = "children")
 public class Card extends VBox {
 	@FXML
-	private Label textField;
-	@FXML
 	private ImageView img;
+	@FXML
+	private Label pokemonName;
+	@FXML
+	private HBox pokemonType;
 
 	private String imgUrl;
 
-	public Card() {
+	public Card(@NamedArg("name") String name, @NamedArg("types") String types) {
 		loadFXML(this);
-		img.setOnMouseClicked((MouseEvent e)->{
+		pokemonName.setText(name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase());
+		insertTypes(types);
+		img.setOnMouseClicked(t -> {
 			bug();
 		});
 	}
 
-	public void setImgUrl(String url){
+	// custom property
+
+	public void setImgUrl(String url) {
 		this.imgUrl = url;
 		img.setImage(new Image(url));
 	}
-	public String getImgUrl(){
+
+	public String getImgUrl() {
 		return this.imgUrl;
 	}
 
+	private void insertTypes(String types) {
+		String[] typeList = types.split(";");
+		Label a = createType(typeList[0].toUpperCase());
+		a.getStyleClass().add("pokemon-type");
+		a.getStyleClass().add(typeList[0]);
+		System.out.println(a.getStyleClass());
+		pokemonType.getChildren().add(a);
+		HBox.setHgrow(a, Priority.ALWAYS);
 
-	public String getText() {
-		return textProperty().get();
+		if (typeList.length == 2) {
+			a = createType(typeList[1].toUpperCase());
+			pokemonType.getChildren().add(a);
+			a.getStyleClass().add("pokemon-type");
+			a.getStyleClass().add(typeList[1]);
+		}
+
+		System.out.println(types);
 	}
 
-	public void setText(String value) {
-		textProperty().set(value);
-	}
-
-	public StringProperty textProperty() {
-		return textField.textProperty();
+	private Label createType(String type) {
+		Label label = new Label();
+		label.setText(type);
+		return label;
 	}
 
 	@FXML
-	protected void doSth() {
-		System.out.println("The button was clicked!" + getText());
-	}
-	@FXML
-	protected void bug(){
-		String newImageString = String.format("https://assets.pokemon.com/assets/cms2/img/pokedex/full/%03d.png",(int) Math.ceil(Math.random()*800));
+	protected void bug() {
+		String newImageString = String.format("https://assets.pokemon.com/assets/cms2/img/pokedex/full/%03d.png", (int) Math.ceil(Math.random() * 800));
 		setImgUrl(newImageString);
 	}
+
 	public static <T extends Parent> void loadFXML(T component) {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setRoot(component);
