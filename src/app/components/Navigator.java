5b0ca3915b330f41a56d9e29232d600d272b2a1f
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-13 17:46:56 +0700

    split detail into smaller component such as navigator

--- src/app/components/Navigator.java
+++ src/app/components/Navigator.java
@@ -1,9 +1,21 @@
 package app.components;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.HBox;
 
-public class Navigator extends HBox {
-	public Navigator(){
+import java.io.IOException;
 
+public class Navigator extends HBox {
+
+	public Navigator() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setRoot(this);
+		loader.setControllerFactory((empty) -> this);
+		String filename = this.getClass().getSimpleName();
+		try {
+			loader.load(this.getClass().getResourceAsStream(String.format("%s.fxml", filename)));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
