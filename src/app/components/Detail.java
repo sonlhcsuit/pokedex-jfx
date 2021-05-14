09ba1b935bceffa75359d7313c43b2679dff6ee5
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-14 13:38:44 +0700

    add stat, add component, etc

--- src/app/components/Detail.java
+++ src/app/components/Detail.java
@@ -3,17 +3,25 @@ package app.components;
 import javafx.beans.NamedArg;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Vector;
 
 
 public class Detail extends BorderPane {
-
+	@FXML
+	private VBox cont;
+	@FXML
+	private HBox types;
 	@FXML
 	private Label name;
 	@FXML
@@ -25,7 +33,9 @@ public class Detail extends BorderPane {
 
 	private Integer pokemonId;
 
-	public Detail(@NamedArg("name") String name, @NamedArg("number") int number, @NamedArg("image") String image) {
+	public Detail(@NamedArg("name") String name, @NamedArg("number") int number,
+				  @NamedArg("image") String image, @NamedArg("types") String types,
+				  @NamedArg("stats") String stats) {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setRoot(this);
 		loader.setControllerFactory((empty) -> this);
@@ -40,18 +50,69 @@ public class Detail extends BorderPane {
 		this.number.setText(String.format("%03d", number));
 		Image img = new Image(image);
 		this.image.setImage(img);
+
+		String[] types_arr = types.split(";");
+		Vector<String> types_vec = new Vector<>();
+		Collections.addAll(types_vec, types_arr);
+		updateTypes(types_vec);
+
+
+		String[] stat_arr = stats.split(";");
+		Vector<Integer> stat_vec = new Vector<>();
+		for (String stat : stat_arr) {
+			stat_vec.add(Integer.parseInt(stat));
+		}
+		System.out.println(stat_vec);
+		updateStat(stat_vec);
+
 	}
 
 	public void updateStat(Vector<Integer> stats) {
-		System.out.println(stats);
+		Vector<Stat> sts = new Vector<>();
+		String[] stat_names = {"hp", "attack", "defense", "sp.atk", "sp.def", "speed"};
+		for (int i = 0; i < stat_names.length; i++) {
+			Stat stat = new Stat(stat_names[i], stats.get(i), 150);
+			if (i == 0) {
+				VBox.setMargin(stat, new Insets(30, 0, 10, 0));
+			}
+			if (i == 5) {
+				VBox.setMargin(stat, new Insets(10, 0, 20, 0));
+			}
+			if (i != 0 && i != 5) {
+				System.out.println(i);
+				VBox.setMargin(stat, new Insets(10, 0, 10, 0));
+			}
+			try {
+				this.cont.getChildren().set(i + 1, stat);
+			} catch (Exception e) {
+				this.cont.getChildren().add(i + 1, stat);
+
+			}
+		}
+
 	}
 
 	public void updateImage(Integer id) {
 		System.out.println(id);
 	}
 
-	public void updateTypes(String types) {
 
+	private Label createType(String type) {
+		Label label = new Label();
+		label.setText(type.toUpperCase());
+		label.setMinWidth(100);
+		Insets margin = new Insets(0, 15, 0, 15);
+		HBox.setMargin(label, margin);
+		label.getStyleClass().add("pokemon-type");
+		label.getStyleClass().add(type);
+		label.setAlignment(Pos.CENTER);
+		return label;
+	}
+
+	public void updateTypes(Vector<String> types) {
+		for (int i = 0; i < 2; i++) {
+			this.types.getChildren().add(createType(types.get(i)));
+		}
 	}
 
 	public void setPokemonId(Integer pokemonId) {
